bef70ac3fe30fd529904246f81f3e1b1c46c4a09
kuldeep <kuldeepyadav7291@gmail.com>
2021-08-18 23:12:07 +0530

    Add orders svc database

--- db-init/app_setup.sql
+++ db-init/app_setup.sql
@@ -1,5 +1,6 @@
 create database if not exists `inventory-svc`;
 create database if not exists `web-svc`;
+create database if not exists `orders-svc`
 
 create user `db-user`@`%` identified by 'P@swr11d';
 grant all privileges on *.* to `db-user`@`%`;
