0674bd31be6fdad939b70dd80f10aebb04b05af0
kuldeep <kuldeep.yadav@noosyntech.in>
2021-09-12 17:24:07 +0530

    Change List to Set for customer address

--- client-web-svc/src/main/java/com/coding/saga/web/domain/Customer.java
+++ client-web-svc/src/main/java/com/coding/saga/web/domain/Customer.java
@@ -1,9 +1,9 @@
 package com.coding.saga.web.domain;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 import static java.util.Objects.requireNonNull;
 
@@ -11,7 +11,7 @@ public class Customer {
     private final CustomerId id;
     private Name name;
     private final String email;
-    private final List<Address> addresses = new ArrayList<>();
+    private final Set<Address> addresses = new HashSet<>();
     private final Cart cart = new Cart();
 
     public Customer(CustomerId id, Name name, String email) {
